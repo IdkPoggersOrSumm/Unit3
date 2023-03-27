e3edff9de7a71037aa42d1521586a38a80596103
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2023-03-27 11:40:42 -0700

    Continued

--- Unit3/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit3/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +26,17 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
         }
     }
-    private void  OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
