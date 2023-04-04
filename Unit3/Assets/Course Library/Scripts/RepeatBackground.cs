e30162bb5eca2f4151977f56c3f3dffa363b2217
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2023-04-04 11:40:22 -0700

    Finished, Working on bonuses

--- Unit3/Assets/Course Library/Scripts/RepeatBackground.cs
+++ Unit3/Assets/Course Library/Scripts/RepeatBackground.cs
@@ -10,14 +10,14 @@ public class RepeatBackground : MonoBehaviour
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+     repeatWidth = GetComponent<BoxCollider>().size.x / 2;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x - repeatWidth)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
--- Unit3/Assets/Course Library/Scripts/SpawnManager.cs
+++ Unit3/Assets/Course Library/Scripts/SpawnManager.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs;
+    private PlayerController playerControllerScript;
     public GameObject obstaclePrefab;
-    public Vector3 spawnPos = new Vector3(30, 0, 0);
+    public Vector3 spawnPos = new Vector3(30, 1, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private int randomObstacle;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate); 
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerController>();
         
     }
 
     // Update is called once per frame
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+        }
+        
     }
 }
